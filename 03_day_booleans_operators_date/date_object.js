503f082d978ab12e1a1adc8d9b6c454966d55e4b
lzk <1714476383@qq.com>
2024-03-21 14:55:06 +0800

    add: date object

---
+++ 03_day_booleans_operators_date/date_object.js
@@ -0,0 +1,11 @@
+let now = new Date();
+console.log(now);
+
+console.log(`fullyear: ${now.getFullYear()}`);
+console.log(`month: ${now.getMonth()}`);
+// Sunday is 0, Monday is 1 and Saturday is 6
+console.log(`day: ${now.getDay()}`);
+console.log(`date: ${now.getDate()}`);
+console.log(`hours: ${now.getHours()}`);
+console.log(`seconds: ${now.getSeconds()}`);
+console.log(now.getTime());
---
+++ 03_day_booleans_operators_date/false_values.js
@@ -0,0 +1,15 @@
+console.log(0 == false);
+console.log(0n == false);
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(false == false);
+console.log('' == false);
+console.log("" == false);
+console.log(`` == false);
+
+let firstName;
+console.log(firstName);
+
+let empty = null;
+console.log(empty);
